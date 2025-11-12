f73cfb2f631f414f9238b9f3e758d4398cfec5cb
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-11-11 22:19:51 -0300

    Criada classe CursoSave

--- prg03TailaMPersistence/src/main/java/br/com/ifba/CursoSave.java
+++ prg03TailaMPersistence/src/main/java/br/com/ifba/CursoSave.java
@@ -4,10 +4,42 @@
  */
 package br.com.ifba;
 
+import br.com.ifba.curso.entity.Curso;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 /**
  *
  * @author Taila
  */
 public class CursoSave {
-    
+
+    // O valor de persistenceUnitName é o mesmo definido 
+    // no persistence.xml
+    private final static EntityManagerFactory entityManagerFactory = 
+        Persistence.createEntityManagerFactory("gerenciamento_curso");
+    private final static EntityManager entityManager = 
+        entityManagerFactory.createEntityManager();
+
+    public static void main(String[] args) {
+
+        Curso curso = new Curso();
+        curso.setId(1L);
+        curso.setNome("Analise e Desenvolvimento de Sistemas");
+        curso.setCodigoCurso("ADS");
+        curso.setAtivo(true);
+
+        try {
+            //SALVANDO UM CURSO
+            entityManager.getTransaction().begin();
+            entityManager.persist(curso);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+        }
+
+        // FECHANDO OS RECURSOS
+        entityManager.close();
+        entityManagerFactory.close();
+    }
 }
+
